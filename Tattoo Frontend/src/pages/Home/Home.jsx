26b2313071f87fbf857f99e000906a00121db51d
angelaejarque <angelaejar@gmail.com>
2024-02-17 16:30:14 +0100

    creado primer input de prueba

--- Tattoo Frontend/src/pages/Home/Home.jsx
+++ Tattoo Frontend/src/pages/Home/Home.jsx
@@ -1,1 +1,22 @@
-export const Home = () =>  {}
+import {useEffect} from "react"
+import {useState} from "react"
+
+
+
+export const Home = () =>  {
+
+    const [inputValue, setInputValue] = useState ('')
+
+    const inputHandler =(e) => {
+        setInputValue(e.target.value)
+    }
+
+    useEffect(()=> {
+        console.log(inputValue)
+    }, [inputValue])
+
+    return (
+      <input name="userName" type="text" placeholder="introduce tu nombre" onChange={(e)=> inputHandler(e)} ></input>
+    )
+
+}
